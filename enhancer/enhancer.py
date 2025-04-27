953ec050bdc0a49fe9a03be115851e27f054697f
kuchipudikavya <133935945+kuchipudikavya@users.noreply.github.com>
2025-04-27 14:26:10 +0530

    Add files via upload

---
+++ enhancer/enhancer.py
@@ -0,0 +1,8 @@
+class Enhancer:
+    def __init__(self, method, background_enhancement, upscale):
+        self.method = method
+        self.background_enhancement = background_enhancement
+        self.upscale = upscale
+
+    def enhance(self, image):
+        return image
